5364e3e3bd3ca6e174dc9166c672b03694692776
kmartin <kmartin@feelagile.com>
2026-02-12 14:50:22 +0100

    map manipulation

--- src/main/java/fr/esgi/Main.java
+++ src/main/java/fr/esgi/Main.java
@@ -8,8 +8,10 @@ public class Main {
 
         Log log = new Log();
         log.setLevel(LEVEL.INFO);
+        log.setMessage("Log numero 1");
         Log log2 = new Log();
         log2.setLevel(LEVEL.INFO);
+        log2.setMessage("Log numero 2");
 
         log.setLevel(LEVEL.INFO);
         List<Log> logList = new ArrayList<>();
@@ -17,19 +19,24 @@ public class Main {
         logList.add(log2);
         logList.add(log);
 
-        System.out.println(logList);
-
         Set<Log> logSet = new HashSet<>();
         logSet.add(log);
         logSet.add(log2);
-        System.out.println(logSet.add(log));
-        System.out.println(logSet);
-
 
         Map<LEVEL, Integer> logByLevel = new HashMap<>();
-        logByLevel.put(LEVEL.INFO, 10);
-        logByLevel.put(LEVEL.WARN, 6);
-        logByLevel.put(LEVEL.ERROR, 4012);
+
+        // parcourir notre liste de logs
+        // alimenter la map pour avoir des statistiques sur les logs
+        // en fonction de leur LEVEL
+
+        for (Log current : logList) {
+            var tmp = logByLevel.get(current.getLevel());
+            if(tmp == null) {
+                tmp = 0;
+            }
+            logByLevel.put(current.getLevel(), tmp + 1);
+            System.out.println("ok tg");
+        }
 
         System.out.println(logByLevel);
     }
